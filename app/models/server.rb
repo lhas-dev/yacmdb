d11eba908f34400c47783fa8e525d4b1cf0c4bff
Luiz Almeida <luizhrqas@gmail.com>
2022-01-12 01:16:43 -0300

    update tabs

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,4 +2,10 @@ class Server < ApplicationRecord
     has_many :hardwares
     has_many :server_softwares
     has_many :server_people
+
+    def owner
+        people = self.server_people.find{|server_people| server_people.role == 'Owner'}
+
+        return people ? people.people.name : 'Empty'
+    end
 end
